cc069717f6701e0186a6ed56e8613144721621e8
Jason Dew <jason.dew@gmail.com>
2022-05-14 20:37:36 -0400

    Project 4 done

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,90 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(LOOP)
+// if RAM[KBD] != 0 GOTO KEYDOWN else GOTO LOOP
+@KBD
+D=M
+@KEYDOWN
+D;JNE
+@LOOP
+0;JMP
+
+(KEYDOWN)
+// RAM[0] = KEYDOWN_LOOP
+@KEYDOWN_LOOP
+D=A
+@R0
+M=D
+// GOTO FILL_BLACK
+@FILL_BLACK
+0;JMP
+
+(KEYDOWN_LOOP)
+// if RAM[KBD] == 0 GOTO KEYUP else GOTO KEYDOWN_LOOP
+@KBD
+D=M
+@KEYUP
+D;JEQ
+@KEYDOWN_LOOP
+0;JMP
+
+(KEYUP)
+// RAM[0] = LOOP
+@LOOP
+D=A
+@R0
+M=D
+// GOTO FILL_WHITE
+@FILL_WHITE
+0;JMP
+
+(FILL_BLACK)
+// D = 8191
+@8191
+D=A
+
+(FILL_BLACK_LOOP)
+// RAM[SCREEN + D] -= 1
+@SCREEN
+A=A+D
+M=M-1
+// D -= 1
+D=D-1
+
+// if D < 0 GOTO FILL_BLACK_DONE else GOTO FILL_BLACK_LOOP
+@FILL_BLACK_DONE
+D;JLT
+@FILL_BLACK_LOOP
+0;JMP
+
+(FILL_BLACK_DONE)
+// GOTO RAM[0]
+@R0
+A=M
+0;JMP
+
+(FILL_WHITE)
+// D = 8191
+@8191
+D=A
+
+(FILL_WHITE_LOOP)
+// RAM[SCREEN + D] += 1
+@SCREEN
+A=A+D
+M=M+1
+// D -= 1
+D=D-1
+
+// if D < 0 GOTO FILL_WHITE_DONE else GOTO FILL_WHITE_LOOP
+@FILL_WHITE_DONE
+D;JLT
+@FILL_WHITE_LOOP
+0;JMP
+
+(FILL_WHITE_DONE)
+// GOTO RAM[0]
+@R0
+A=M
+0;JMP
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,51 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+// R2 = 0
+@R2
+M=0
+
+// GOTO END if R0 = 0
+@R0
+M;JEQ
+
+// GOTO END if R1 = 0
+@R1
+M;JEQ
+
+// R2 = R0
+@R0
+D=M
+@R2
+M=D
+
+// i = R1
+@R1
+D=M
+@i
+M=D
+
+// while i > 1
+(LOOP)
+@i
+D=M-1
+@END
+D;JLE
+
+// R2 += R0
+@R0
+D=M
+@R2
+M=M+D
+
+// i -= 1
+@i
+M=M-1
+
+// GOTO LOOP
+@LOOP
+0;JMP
+
+(END)
+@END
+0;JMP
